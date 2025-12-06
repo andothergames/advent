2b728cc89225ab22f30927b3a5ee0d01eda788d7
andothergames <32204909+andothergames@users.noreply.github.com>
2025-12-06 07:56:25 +0000

    day 5 part 2

--- 2015/05/0515.py
+++ 2015/05/0515.py
@@ -40,4 +40,45 @@ def countNice(data):
     print(count)
 
     
-countNice(data)
+# countNice(data)
+
+# part two
+
+def checkNonOverlappingDoubles(string):
+    for i in range(len(string) - 2):
+        if (string[i:i+2] in string[i+2:]):
+            return True
+    return False
+
+def checkNextDoorButOneNeighbours(string):
+    for i in range(len(string) - 2):
+        if (string[i] == string[i+2]):
+            return True
+    return False
+
+def checkNewCount(data):
+    count = 0
+    for _ in data:
+        if checkNonOverlappingDoubles(_) and checkNextDoorButOneNeighbours(_):
+            count += 1
+    print(count)
+
+
+
+checkNewCount(data)
+
+
+
+
+
+# Now, a nice string is one with all of the following properties:
+
+# It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+# It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+# For example:
+
+# qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that repeats with exactly one letter between them (zxz).
+# xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.
+# uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.
+# ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.
+# How many strings are nice under these new rules?
