57cd73020e6bf2ac598679fb2c293032278f49e9
andothergames <32204909+andothergames@users.noreply.github.com>
2025-12-14 18:46:36 +0000

    2025: day 2 part 2 wip

--- 2025/02/repeatedDigits.py
+++ 2025/02/repeatedDigits.py
@@ -22,26 +22,74 @@ def isRepeatedDigit(n):
     return checker
 
 
-def formatRange(code):
+def formatRange(code, method):
     range = code.split('-')
     number = int(range[0])
     end = int(range[1])
     total = 0
-    while number < end:
-        number += 1
-        if isRepeatedDigit(number):
-            total += number
-        if hasRepeatedDigits(number):
-            total += number
+    if method == 1:
+        while number < end:
+            number += 1
+            if isRepeatedDigit(number):
+                total += number
+            if hasRepeatedDigits(number):
+                total += number
+    if method == 2:
+        while number < end:
+            number += 1
+            if isRepeatedDigit(number):
+                total += number
+
     return total
 
 
-def main(code):
+def findFactors(x):
+    factors = []
+    for i in range(1, x):
+       if x % i == 0:
+           factors.append(i)
+    factors.pop(0)
+    return factors
+
+
+def repeatedDigits(n):
+    strNum = str(n)
+    number = len(strNum)
+    factors = findFactors(number)
+    match = False
+    
+    for x in factors:
+        start = 0
+        end = x
+        y = 0
+        segment = strNum[start:end]
+        if match == True:
+            return match
+        while y < len(strNum):
+            print(strNum[start:end])
+            if strNum[start:end] == segment:
+                match = True
+            else:
+                match = False
+            end += x
+            start += x
+            y += x
+
+    return match
+
+
+
+def main(code, method):
     IDtotal = 0
-    for range in code:
-        total = formatRange(range)
-        IDtotal += total
+    if method == 1:
+        for range in code:
+            total = formatRange(range)
+            IDtotal += total
+    if method == 2:
+        for range in code:
+            total = repeatedDigits(range)
+            IDtotal += total
     return IDtotal
 
 
-print(main(IDs))
+# print(main(IDs))
