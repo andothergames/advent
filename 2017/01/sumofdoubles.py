161d6f6febdcfa78133f947bc401c8b74325442c
andothergames <32204909+andothergames@users.noreply.github.com>
2025-12-28 00:33:08 +0000

    2017: 1.2

--- 2017/01/sumofdoubles.py
+++ 2017/01/sumofdoubles.py
@@ -1,15 +1,25 @@
 with open('0117input.txt', 'r') as inputfile:
     numbers = list(inputfile.read())
 
-total = 0
+total1 = 0
 i = 0
 
 while i < len(numbers) - 1:
     if numbers[i] == numbers[i + 1]:
-        total += int(numbers[i])
+        total1 += int(numbers[i])
     i += 1
 
 if numbers[0] == numbers[-1]:
-    total += int(numbers[0])
+    total1 += int(numbers[0])
 
-print(total)
+# print('1.1', total)
+
+total1 = 0
+length = len(numbers)
+halfway = length // 2
+
+for j in range(length):
+    if numbers[j] == numbers[(j + halfway) % length]:
+        total1 += int(numbers[j])
+
+# print(total1)
